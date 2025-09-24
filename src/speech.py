003948ac98c609b58ff47df4794d64e8ca92c643
s10boi <avonpup@gmail.com>
2025-09-24 10:58:56 +0100

    Dependency, pre-commit and CI updates

--- src/speech.py
+++ src/speech.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 from typing import cast
 
-import pyttsx3
+import pyttsx3  # pyright: ignore[reportMissingTypeStubs]
 
 
 def export_audio(*, text: str, export_filepath: Path) -> None:
     """Exports the given text to an mp3 audio file at the given path."""
-    engine = cast("pyttsx3.Engine", pyttsx3.init())  # pyright: ignore reportUnknownVariableType
+    engine = cast("pyttsx3.Engine", pyttsx3.init())
     engine.setProperty("rate", 110)
     engine.setProperty("volume", 0.8)
     engine.save_to_file(text, str(export_filepath))
